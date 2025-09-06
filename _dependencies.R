153f78e52b4bf51f6d6d40e16494bafed00a185f
Andrew Heiss <andrew@andrewheiss.com>
2025-09-05 22:46:57 -0400

    {renv} time

---
+++ _dependencies.R
@@ -0,0 +1,7 @@
+# Things that need to be installed but didn't get installed automatically as 
+# dependencies because {renv} couldn't see them
+library(quarto)
+library(collapse)
+library(cmdstanr)
+library(yaml)
+library(litedown)
